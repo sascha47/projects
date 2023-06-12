d61c460ae571b7cad869a3c7cc05a6e96cdc1c4e
floki47 <sascha.west@bellevuecollege.edu>
2023-06-11 19:13:17 -0700

    added image scraping .png

--- scraping/pdf_scraper.py
+++ scraping/pdf_scraper.py
@@ -7,6 +7,7 @@ import sys
 
 link = str(sys.argv[1])
 # https://www.bellevuecollege.edu/publications/
+#https://www.bellevuecollege.edu/careers/
 # get the HTML content
 response = requests.get(link)
 
@@ -23,16 +24,31 @@ a_tags = soup.find_all('a')
 # final all links ending in .pdf
 pdf_links = soup.find_all('a', href=lambda x: x and '.pdf' in x)
 
+#find all links ending in .jpg
+pic_links = soup.find_all('img',src=lambda x: x and ".png" in x)
+
+for links in pic_links:
+    pic_links = links.get('src')
+    pic_response = requests.get(pic_links)
+    pic_name = os.path.split(pic_links)[-1]
+    with open(pic_name,'wb') as file:
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                file.write(chunk)
+
 #download the pdf files
 for links in pdf_links:
     #print(link.get('href'))
     pdf_url = links.get('href')
+    
 
     # send a GET request to the pdf URL
     pdf_response = requests.get(pdf_url)
+    
 
     # The PDF file name will be the last part of the url
     pdf_name = os.path.split(pdf_url)[-1]
+    
 
     # we write the content of the response to the file 
     # same name as the file
@@ -41,5 +57,7 @@ for links in pdf_links:
         for chunk in response.iter_content(chunk_size=1024):
             if chunk:
                 file.write(chunk)
+
+    
     
    
