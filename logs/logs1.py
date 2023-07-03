62c57c2c27f3ac0be9a7bda3d1131aa56f3b3ed4
sascha47 <sascha.west@bellevuecollege.edu>
2023-07-02 18:30:59 -0700

    tracking macs from iphones requesting dhcp

--- logs/logs1.py
+++ logs/logs1.py
@@ -5,7 +5,10 @@ array = []
 
 with open("dhcpdsmall.log","r") as file:
     for line in file:
-        m = re.search("((?:\w{2}:){5}\w{2}).\(iPhone\)",line)
+        m = re.search("((?:\w{2}:){5}\w{2})\s+\((iPhone|Alans-iPhone)\)",line,re.DOTALL)
         if m:
             array.append(m.group(1))
-    print(array)
+    #print(array)
+
+single=set(array)
+print(single)
