9b8f4dc1ccd3a3869616a39d6231ad7cc0478771
sascha47 <sascha.west@bellevuecollege.edu>
2023-07-02 17:55:02 -0700

    logs

--- logs/logs1.py
+++ logs/logs1.py
@@ -1,11 +1,11 @@
 import sys
 import re
 
-mac_address = set()
+array = []
 
 with open("dhcpdsmall.log","r") as file:
     for line in file:
         m = re.search("((?:\w{2}:){5}\w{2}).\(iPhone\)",line)
         if m:
-            mac_address.append(m.group())
-    print(mac_address)
+            array.append(m.group(1))
+    print(array)
